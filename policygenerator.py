3a9ad0293da47a1f56a3a9281a2592c50b47466a
Vinood Katuwa <vinod.katuwa12@gmail.com>
2018-03-21 01:04:42 +0530

    Update policygenerator.py

--- policygenerator.py
+++ policygenerator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
-import boto3
+# import boto3
 import argparse
 import json
+import os
 from pprint import pprint
 
 def parse_arguments():
@@ -17,6 +18,28 @@ def parse_arguments():
                       default="services")
   return parser.parse_args()
 
+
+def merge_json(input_files,output_file="output.json"):
+    # read json file from current dir
+    for file in input_files:
+        file = file + ".json"
+        if os.path.isfile(file):
+            # Read file content
+            with open(file,"r") as f:
+                file_content = json.load(f)
+            # Write file content
+            with open(output_file,"a") as json_file:
+                json.dump(file_content, json_file,indent=4, sort_keys=True)
+            print "file has been updated"
+        else:
+            print "Policy does not exists"
+
+                # Update Region
+                # if "Resource" in file_content:
+                #    print file_content
+
+
+
 if __name__ == '__main__':
 
   args = parse_arguments()
@@ -25,27 +48,4 @@ if __name__ == '__main__':
 
   print args.region
   print args.component
-
-  my_big_var = []
-  for my_service in my_services:
-      my_content = json.load(open(my_service + ".json" ))
-      my_big_var.append(my_content)
-
-
-
-  pprint(my_big_var)
-
-  print my_service
-
-  #my_content = json.load(open(my_service))
-  print(my_content)
-  
-  with open('output.json', 'w') as outfile:
-    json.dump(my_content, outfile)
-
-  #if("Resource" in my_content):
-     #for my_resource in my_content["Resource"]:
-       #if("REGION_HERE" in my_resource):
-         #my_resource.replace("REGION_HERE", args.region)
-
-  #print(my_content)
+  merge_json(my_services)
