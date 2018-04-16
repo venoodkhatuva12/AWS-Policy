841e8abf78a4d8ee29d3b0b057e213369164be5c
Vinood Katuwa <vinod.katuwa12@gmail.com>
2018-04-16 15:48:26 +0530

    Update policygenerator.py

--- scripts/policygenerator.py
+++ scripts/policygenerator.py
@@ -32,9 +32,6 @@ def merge_json(input_files,output_file="output.json"):
              }
     for file in input_files:
     	file = file + ".json"
-    		if file = "elasticcache.json"
-    				print "use service link policy cant use managed policy"
-    		else:
         if os.path.isfile(file):
             # Read file content
             with open(file,"r") as f:
